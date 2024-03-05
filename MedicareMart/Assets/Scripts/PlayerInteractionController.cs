f5a4d62130727181e6052e51bab52846d5630063
dduwa <99512283+dduwa@users.noreply.github.com>
2024-03-05 21:07:37 +0000

    added crosshair

---
+++ MedicareMart/Assets/Scripts/PlayerInteractionController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine;
+public class PlayerInteractionController : MonoBehaviour
+{
+    void Update()
+    {
+        PhysicsRaycasts();
+    }
+    void PhysicsRaycasts()
+    {
+        Vector3 centreOfScreen =
+        new Vector3(Screen.width * 0.5f, Screen.height * 0.5f, 0);
+        float distanceToFireRay = 20;
+        Ray centreOfScreenRay =
+        Camera.main.ScreenPointToRay(centreOfScreen);
+        RaycastHit hit;
+        if (Physics.Raycast(centreOfScreenRay, out hit, distanceToFireRay))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Debug.Log("Raycast hit: " + hit.transform.name);
+            }
+        }
+    }
+}
