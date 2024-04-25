98b825583695ff5a8d1fc870babdf55f6c7b533d
dduwa <99512283+dduwa@users.noreply.github.com>
2024-04-26 00:25:48 +0100

    added sound to doors opening

--- MedicareMart/Assets/Scripts/StoreDoorControllers.cs
+++ MedicareMart/Assets/Scripts/StoreDoorControllers.cs
@@ -5,17 +5,31 @@ using UnityEngine;
 public class StoreDoorControllers : MonoBehaviour
 {
     public Animator animator;
+    public AudioSource audioSource; // AudioSource component to play sounds
+    public AudioClip doorSound; // AudioClip for door
 
     void Start()
     {
         animator = GetComponentInParent<Animator>();
+        audioSource = GetComponent<AudioSource>(); // Ensure there's an AudioSource component
+
         if (animator == null) Debug.LogError("Animator not found on the door!");
+        if (audioSource == null) Debug.LogError("AudioSource not found on the door!");
     }
 
     public void ToggleDoor()
     {
         bool isOpen = animator.GetBool("IsOpen");
         animator.SetBool("IsOpen", !isOpen);
+        PlaySound();
+    }
+
+    void PlaySound()
+    {
+        if (audioSource != null && doorSound != null)
+        {
+            audioSource.PlayOneShot(doorSound);
+        }
     }
 
 }
