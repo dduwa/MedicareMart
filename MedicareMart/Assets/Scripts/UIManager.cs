45b1ca2d3f865f00e94bca958c7e73b98efcec49
dduwa <99512283+dduwa@users.noreply.github.com>
2024-05-06 13:00:53 +0100

    Revert "fixed ui manager"
    This reverts commit e92b5b665d69d150df1f75981e6ac0c01cd4b4bb.

--- MedicareMart/Assets/Scripts/UIManager.cs
+++ MedicareMart/Assets/Scripts/UIManager.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
+
 
 public class UIManager : MonoBehaviour
 {
@@ -9,8 +9,8 @@ public class UIManager : MonoBehaviour
 
     public GameObject crosshair;
     public ObjectivesController objectivesManager;
-    public GameObject pausePanel;
-    public GameObject confirmPanel; // Assign in the Inspector
+    public GameObject pauseMenuUI;
+    public GameObject confirmationDialog; // Assign in the Inspector
     public Text messageText; // Assign in the Inspector
     public Button yesButton, noButton; // Assign in the Inspector
     public Button resumeButton; // Assign in the Inspector
@@ -23,7 +23,6 @@ public class UIManager : MonoBehaviour
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            SceneManager.sceneLoaded += OnSceneLoaded;
         }
         else
         {
@@ -33,51 +32,13 @@ public class UIManager : MonoBehaviour
     void Start()
     {
         // Disable the Pause Menu UI GameObject which should be assigned in the inspector.
-        if (pausePanel != null)
-            pausePanel.SetActive(false);
+        if (pauseMenuUI != null)
+            pauseMenuUI.SetActive(false);
 
         // Disable the Confirmation Dialog GameObject which should be assigned in the inspector.
-        if (confirmPanel != null)
-            confirmPanel.SetActive(false);
+        if (confirmationDialog != null)
+            confirmationDialog.SetActive(false);
     }
-
-    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-    {
-        AssignUIComponents();
-    }
-
-    private void AssignUIComponents()
-    {
-        objectivesManager = FindObjectOfType<ObjectivesController>();
-        // Assign UI elements; make sure to check if they exist in the scene
-        crosshair = GameObject.Find("Crosshair");
-        pausePanel = GameObject.Find("PausePanel");
-        confirmPanel = GameObject.Find("ConfirmPanel");
-        messageText = GameObject.FindWithTag("MessageText")?.GetComponent<Text>();
-        yesButton = GameObject.FindWithTag("YesButton")?.GetComponent<Button>();
-        noButton = GameObject.FindWithTag("NoButton")?.GetComponent<Button>();
-        resumeButton = GameObject.FindWithTag("ResumeButton")?.GetComponent<Button>();
-
-        // Check each component and log if not found
-        CheckAndLogComponent(crosshair, "Crosshair");
-        CheckAndLogComponent(pausePanel, "PausePanel");
-        CheckAndLogComponent(confirmPanel, "ConfirmPanel");
-        CheckAndLogComponent(messageText, "MessageText");
-        CheckAndLogComponent(yesButton, "YesButton");
-        CheckAndLogComponent(noButton, "NoButton");
-        CheckAndLogComponent(resumeButton, "ResumeButton");
-
-        // Reset UI visibility as needed
-        if (pausePanel != null) pausePanel.SetActive(false);
-        if (confirmPanel != null) confirmPanel.SetActive(false);
-    }
-
-    private void CheckAndLogComponent(UnityEngine.Object component, string name)
-    {
-        if (component == null)
-            Debug.LogError($"UIManager: {name} not found in the scene.");
-    }
-
 
     public void ToggleCrosshair(bool state)
     {
@@ -106,13 +67,13 @@ public class UIManager : MonoBehaviour
 
     public void ShowPauseMenu()
     {
-        if (pausePanel != null)
+        if (pauseMenuUI != null)
         {
             resumeButton.onClick.RemoveAllListeners();
             resumeButton.onClick.AddListener(() => GameManager.Instance.ResumeGame());
 
 
-            pausePanel.SetActive(true);
+            pauseMenuUI.SetActive(true);
             ToggleCrosshair(false);
             ToggleCursorVisibility(true);
         }
@@ -124,9 +85,9 @@ public class UIManager : MonoBehaviour
 
     public void HidePauseMenu()
     {
-        if (pausePanel != null)
+        if (pauseMenuUI != null)
         {
-            pausePanel.SetActive(false);
+            pauseMenuUI.SetActive(false);
             ToggleCrosshair(true);
             ToggleCursorVisibility(false);
         }
@@ -156,7 +117,7 @@ public class UIManager : MonoBehaviour
             HideConfirmationDialog();
         });
 
-        confirmPanel.SetActive(true);
+        confirmationDialog.SetActive(true);
         ToggleCrosshair(false);
         ToggleCursorVisibility(true);
     }
@@ -166,7 +127,7 @@ public class UIManager : MonoBehaviour
     public void HideConfirmationDialog()
     {
         GameManager.Instance.ResumeGame();
-        confirmPanel.SetActive(false);
+        confirmationDialog.SetActive(false);
         ToggleCrosshair(true);
         ToggleCursorVisibility(false);
     }
